60bfd45e04f2da68516d2ecd1d12f8f428c048ba
lolotrgeek <lolotrgeek@gmail.com>
2020-12-30 10:15:20 -0600

    update for absolute mouse x,y

--- hid/mouse.py
+++ hid/mouse.py
@@ -1,24 +1,10 @@
 from hid import write as hid_write
 
+# Reference:
+# https://github.com/mtlynch/tinypilot/commit/ea853b54fa56ce275b6f776caa5a839451013e84#diff-d589676c00c019dfd703ad4aac7f458ffe5efda9116a930d3474d28aa3def657
+# https://www.raspberrypi.org/forums/viewtopic.php?t=234495
+# https://wiki.osdev.org/Mouse_Input
 
-def send_mouse_event(mouse_path, buttons, relative_x, relative_y,
-                     vertical_wheel_delta, horizontal_wheel_delta):
-    x, y = _scale_mouse_coordinates(relative_x, relative_y)
-
-    buf = [0] * 7
-    buf[0] = buttons
-    buf[1] = x & 0xff
-    buf[2] = (x >> 8) & 0xff
-    buf[3] = y & 0xff
-    buf[4] = (y >> 8) & 0xff
-    buf[5] = vertical_wheel_delta & 0xff
-    buf[6] = horizontal_wheel_delta & 0xff
-    hid_write.write_to_hid_interface(mouse_path, buf)
-
-
-def _scale_mouse_coordinates(relative_x, relative_y):
-    # This comes from LOGICAL_MAXIMUM in the mouse HID descriptor.
-    max_hid_value = 32767.0
-    x = int(relative_x * max_hid_value)
-    y = int(relative_y * max_hid_value)
-    return x, y
+def send_mouse_event(mouse_path, button, dx, dy, wheel):
+    report = [button, dx & 0xff, dy & 0xff, wheel & 0xff]
+    hid_write._write_to_hid_interface(mouse_path, report)
