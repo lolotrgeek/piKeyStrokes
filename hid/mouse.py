c81c4515a35bc9d8bd261735c6773a59c7faa7ea
lolotrgeek <lolotrgeek@gmail.com>
2021-01-12 10:23:36 -0600

    build relative report before send

--- hid/mouse.py
+++ hid/mouse.py
@@ -11,14 +11,6 @@ def send_mouse_event(server_address, button, dx, dy, wheel):
     hid_send.send(server_address, event) 
 
 def send_mouse_event_relative(server_address, button, x, y, wheel, height, width):
-    report = [button, x & 0xff, y & 0xff, wheel & 0xff, height & 0xff, width & 0xff]
-    hid_send.send(server_address, report)
-    
-def write_mouse_event(mouse_path, event):
-    report = event 
-    hid_write._write_to_hid_interface_immediately(mouse_path, report)
-
-def write_mouse_event_relative(mouse_path, event):
     '''
     Scale mouse events with a relative coordinate system.
 
@@ -27,26 +19,31 @@ def write_mouse_event_relative(mouse_path, event):
     This allows the system to adapt to changing screen sizes and convert pixel values to movements
 
     NOTE: requires 2-byte `x , y` inputs, see `enable_hid.sh` `Mouse 2`
-    '''
-    x , y , height, width = event[1], event[2], event[4], event[5]   
-    print(x, y, height, width)
-
-    relative_x = set_relative(x, width)
-    relative_y = set_relative(y, height)
-    scale_x, scale_y = scale_mouse_coordinates(relative_x, relative_y)
-
+    '''    
+    rel_x = relative_pos(x, width)
+    rel_y = relative_pos(y, height)
+    scale_x, scale_y = scale_mouse_coordinates(rel_x, rel_y) 
     report = [0] * 6
-    report[0] = event[0] # button
-    report[1] = scale_x & 0xff 
+    report[0] = button
+    report[1] = scale_x & 0xff
     report[2] = (scale_x >> 8) & 0xff
     report[3] = scale_y & 0xff
     report[4] = (scale_y >> 8) & 0xff
-    report[5] = event[3] # wheel   
+    report[5] = wheel & 0xff    
+    hid_send.send(server_address, report)
+    
+def write_mouse_event(mouse_path, event):
+    report = event 
+    hid_write._write_to_hid_interface_immediately(mouse_path, report)
+
+def write_mouse_event_relative(mouse_path, event):
+
+    report = event
 
     hid_write._write_to_hid_interface_immediately(mouse_path, report)
 
-def set_relative(coord, total):
-    return min(1.0, max(0.0, coord / total))
+def relative_pos(pos, total):
+    return min(1.0, max(0.0, pos / total))
 
 def scale_mouse_coordinates(relative_x, relative_y):
     # This comes from LOGICAL_MAXIMUM in the mouse HID descriptor.
